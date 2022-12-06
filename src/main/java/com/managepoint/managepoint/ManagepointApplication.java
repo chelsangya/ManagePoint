762bcfad4f97ec64086245a4f6a8d96249f977e3
chelsangya <csangya@gmail.com>
2022-12-06 17:08:41 +0545

    Initializing Repository

---
+++ src/main/java/com/managepoint/managepoint/ManagepointApplication.java
@@ -0,0 +1,13 @@
+package com.managepoint.managepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagepointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagepointApplication.class, args);
+	}
+
+}
