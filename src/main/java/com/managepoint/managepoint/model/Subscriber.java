c0a10a40b56173ba34b20f8b366880a9ba9589c6
chelsangya <csangya@gmail.com>
2022-12-27 10:45:33 +0545

    models

--- src/main/java/com/managepoint/managepoint/model/Subscriber.java
+++ src/main/java/com/managepoint/managepoint/model/Subscriber.java
@@ -1,7 +1,11 @@
 package com.managepoint.managepoint.model;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name="subscriber")
 public class Subscriber {
     @Id
@@ -9,14 +13,10 @@ public class Subscriber {
     @GeneratedValue(generator = "subscriber_id_seq_gen", strategy = GenerationType.SEQUENCE)
     @Column(name = "s_id")
     private Integer s_id;
-
     @Column(name="s_name" ,nullable = false)
     private String s_name;
     @Column(name="s_email",nullable = false)
     private String s_email;
-    @ManyToOne
-    @JoinColumn ( name ="ts_id", nullable = false )
-    private Tag tag;
     @ManyToOne
     @JoinColumn ( name ="us_id", nullable = false )
     private User  user;
