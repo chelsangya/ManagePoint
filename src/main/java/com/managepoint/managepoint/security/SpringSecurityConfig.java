cff059bcd958a42cd53c5e447dfc4aa24f118fca
chelsangya <csangya@gmail.com>
2023-01-05 18:26:51 +0545

    changing

--- src/main/java/com/managepoint/managepoint/security/SpringSecurityConfig.java
+++ src/main/java/com/managepoint/managepoint/security/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -28,21 +29,26 @@ public class SpringSecurityConfig {
         return authenticationProvider;
     }
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
+    protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception{
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/homepage/**","/user/**","/admin/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/dashboard", true)
+                .defaultSuccessUrl("/dashboard",true)
+                .usernameParameter("email")
+                .permitAll()
                 .and()
-                .logout()
-                .logoutUrl("/logout")
-                .deleteCookies("JSESSIONID");
-        return http.build();
+                .httpBasic();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().anyRequest();
     }
 }
