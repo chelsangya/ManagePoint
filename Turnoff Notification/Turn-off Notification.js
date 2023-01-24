cbe89d4f35eb5ad544e79cf8617c1bccb4abd26f
chelsangya <csangya@gmail.com>
2023-01-24 15:55:32 +0545

    editing

--- Turnoff Notification/Turn-off Notification.js
+++ Turnoff Notification/Turn-off Notification.js
@@ -16,16 +16,17 @@ function requestAndShowPermission() {
     });
 }
 function showNotification() {
-  //  if(document.visibilityState === "visible") {
-  //      return;
-  //  }
-   let title = "I love Educative.io";
-   let icon = 'https://homepages.cae.wisc.edu/~ece533/images/zelda.png'; //this is a large image may take more time to show notifiction, replace with small size icon
-   let body = "Message to be displayed";
+    //  if(document.visibilityState === "visible") {
+    //      return;
+    //  }
+    let title = "I love Educative.io";
+    let icon = 'https://homepages.cae.wisc.edu/~ece533/images/zelda.png'; //this is a large image may take more time to show notifiction, replace with small size icon
+    let body = "Message to be displayed";
 
-   let notification = new Notification(title, { body, icon });
+    let notification = new Notification(title, {body, icon});
 
-   notification.onclick = () => {
-          notification.close();
-          window.parent.focus();
-   }
+    notification.onclick = () => {
+        notification.close();
+        window.parent.focus();
+    }
+}
