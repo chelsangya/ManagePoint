02703c2ebb9964a827bc47bd731f7159110d96a2
chelsangya <csangya@gmail.com>
2023-01-31 10:36:47 +0545

    redesigning file structure

--- src/main/resources/static/javascript/script.js
+++
@@ -1,9 +0,0 @@
-// menu bar
-
-const navToggle = document.querySelector("#menu-bar");
-const link = document.querySelector(".links");
-
-navToggle.addEventListener("click", function () {
-    navToggle.classList.toggle("fa-times");
-    link.classList.toggle("show_nav");
-});
